f2c1ee26a5bbd418957a15f6360c1e8dbaa07a2b
umair jamaat ali <umairansari758@gmail.com>
2024-02-19 00:01:26 +0500

    heroproduct

--- app/components/heroProduct/HeroProduct.js
+++ app/components/heroProduct/HeroProduct.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './HeroProduct.css'
 import Image from 'next/image'
-import img from '../../../public/assets/watch1.jpg'
+import img from '../../../public/assets/watch1r-.png'
 
 
 
@@ -12,7 +12,7 @@ export default function HeroProduct() {
     <div className='Hero-container-left'>
         <h1>Watch of Choice</h1>
         <p>Enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse.</p>
-        <button className='w-[275px] h-[64px] bg-red-600'>BUY NOW</button>
+        <button className='w-[255px] h-[64px] bg-red-600 text-white hover:bg-red-900 hover:text-black active:bg-amber-600'>BUY NOW</button>
     </div>
     <div className='Hero-container-right'>
     <Image
