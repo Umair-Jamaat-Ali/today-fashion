ed3f650ee10543828b5d4bcb3f2761e6cb193765
umair jamaat ali <umairansari758@gmail.com>
2024-02-20 00:08:21 +0500

    heroproduct

--- app/components/heroProduct/HeroProduct.js
+++ app/components/heroProduct/HeroProduct.js
@@ -10,15 +10,15 @@ export default function HeroProduct() {
     <>
     <div className='Hero-container'>
     <div className='Hero-container-left'>
-        <h1>Watch of Choice</h1>
+        <h1 className='text-3xl font-extrabold'>Watch of Choice</h1>
         <p>Enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse.</p>
         <button className='w-[255px] h-[64px] bg-red-600 text-white hover:bg-red-900 hover:text-black active:bg-amber-600'>BUY NOW</button>
     </div>
     <div className='Hero-container-right'>
     <Image
       src={img}
-      width={500}
-      height={500}
+      width={900}
+      height={900}
       alt="Picture of the watch"
     />
     </div>
